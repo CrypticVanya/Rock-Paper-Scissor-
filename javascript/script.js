907c4cae38c9070c20c3013693a4d6c3db7ec114
Vanya <crypticvanya@gmail.com>
2022-04-17 17:13:01 -0400

    Add the function that will play a round using if..else statement with && logical operator

--- javascript/script.js
+++ javascript/script.js
@@ -1,11 +1,29 @@
 
 const pickOne = (arr) => arr[Math.floor(Math.random() * arr.length)]
-
-let choice = pickOne(["rock", "paper", "scissor"])
+let choice = pickOne(['rock', 'paper', 'scissor'])
+const computerSelection = computerPlay()
 
 function computerPlay() {
-    return choice
+    return choice   
 }
 
-console.log(computerPlay())
+console.log(computerSelection)
 
+const playerSelection = "scissor"
+
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection === 'rock' && computerSelection === 'paper') {
+        console.log("You LOSE, rock is weak against paper")
+    } else if (playerSelection === 'rock' && computerSelection === 'scissor') {
+        console.log("you WIN, rock beats scissor")
+    } else if (playerSelection === 'scissor' && computerSelection === 'paper') {
+        console.log("You WIN, scissors cuts paper")
+    } else if (playerSelection === 'scissor' && computerSelection === 'rock') {
+        console.log("You LOSE, scissor gets smashed by rock")
+    } else {
+        console.log("It is a tie, try again")
+    }
+}
+
+
+let winner = playRound(playerSelection, computerSelection)
