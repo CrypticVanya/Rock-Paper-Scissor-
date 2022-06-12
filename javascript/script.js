700e016069848f9cb59fd5b80d9eabd895dc7d55
Vanya <crypticvanya@gmail.com>
2022-06-11 21:37:33 -0400

    update UI

--- javascript/script.js
+++ javascript/script.js
@@ -1,3 +1,5 @@
+//Create an array that contains the choises of "rock, paper, scissor" game then randomizing a pick with Math.random
+
 let compChoises = ['rock', 'paper', 'scissor']
 let myScore = 1
 let compScore = 1
@@ -7,7 +9,7 @@ function randomCompSelect() {
 }
 
 
-//Create an array that contains the choises of "rock, paper, scissor" game then randomizing a pick with Math.random
+//plays the game when an option is selected
 
 const buttons = document.querySelectorAll('.selection')
 
@@ -20,40 +22,41 @@ buttons.forEach((button) => {
         if (myScore === 6 || compScore === 6){
             gameWinner()
             resetGame()
-        }
-         
+        }    
     });
 });
 
-//Create and obtain the player selection from the ui
+
+//Create and obtain the player selection from the UI
 
 function playRound(playerSelection, computerSelection) {
- 
-       if ((playerSelection === 'rock' && computerSelection === 'scissor') ||
+    if ((playerSelection === 'rock' && computerSelection === 'scissor') ||
         (playerSelection === 'paper' && computerSelection === 'rock') ||
         (playerSelection === 'scissor' && computerSelection === 'paper')) {
-             `${messagEl('Congrats, you have won')} + ${playerScore(myScore++)}}`
-        } else if ((playerSelection === 'rock' && computerSelection === 'paper')||
-                    (playerSelection === 'paper' && computerSelection === 'scissor')||
-                    (playerSelection === 'scissor' && computerSelection === 'rock')) {
-                        `${messagEl('Oh no, You have Lost')} + ${computerScore(compScore++)}`
-                    } else {
-                         `${messagEl('try again, it is a tie')}`
-                    } 
-       
+            `${messagEl('Congrats, you get a point')} + ${playerScore(myScore++)}}`
+    } else if ((playerSelection === 'rock' && computerSelection === 'paper')||
+        (playerSelection === 'paper' && computerSelection === 'scissor')||
+        (playerSelection === 'scissor' && computerSelection === 'rock')) {
+            `${messagEl('Oh no, enemy gets a point')} + ${computerScore(compScore++)}`
+    } else {
+            `${messagEl('Try again, it is a tie')}`
+    } 
 }
 
 
-//plays one round using playerSelect and computerSelect
+//function for declaration messages
 
 function messagEl(message) {
-const displayMessage = document.querySelector('#display-message')
-displayMessage.textContent = message;
+    const displayMessage = document.querySelector('#display-message')
+    displayMessage.textContent = message;
 }
 
+
+// functions to keep track of score
+
 function playerScore(myScore) {
-const keepPlayerScore = document.querySelector('#player')
-keepPlayerScore.textContent = myScore
+    const keepPlayerScore = document.querySelector('#player')
+    keepPlayerScore.textContent = myScore
 }
 
 function computerScore(compScore) {
@@ -61,14 +64,18 @@ function computerScore(compScore) {
     keepComputerScore.textContent = compScore
 }
 
+
+// function to declare the winner
+
 function gameWinner() {
     if (myScore > compScore) {
-        messagEl("Congrats, you have won the game!")
+        winningEl("Congrats, you won the game!")
     } else {
-        messagEl("Sorry, you have lost the game!")
+        losingEl("Sorry, you have lost the game!")
     }
 }
 
+// function that resets the game
 
 function resetGame() {
     myScore = 1
@@ -76,3 +83,16 @@ function resetGame() {
     computerScore(0)
     playerScore(0)
 }
+
+//function to create winning message
+
+function winningEl(message) {
+    const displayMessage = document.querySelector('#display-message');
+    displayMessage.textContent = message;
+}
+
+function losingEl(message) {
+    const displayMessage = document.querySelector('#display-message');
+    displayMessage.textContent = message;
+}
+    
